4e50f2b931fb9e75e837d018ca3ac2809e92f33d
Jonas <jonas@labsit.io>
2021-04-06 23:03:10 -0300

    Implementando o Ribbon para balancear requisições

--- hr-payroll/src/main/java/com/spring/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/spring/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,11 @@ package com.spring.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+@RibbonClient(name = "hr-worker") // Define o projeto como um client do Ribbon
+@EnableFeignClients // Habilitar o FeignClient
 @SpringBootApplication
 public class HrPayrollApplication {
 
