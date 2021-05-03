fea74713359a0c5a81b0bcb6b1aff80d59219aed
Jonas <jonas@labsit.io>
2021-05-03 10:08:20 -0300

    IMplementando o actuator refresh

--- hr-worker/src/main/java/com/spring/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/spring/hrworker/resources/WorkerResource.java
@@ -7,6 +7,8 @@ import com.spring.hrworker.repositories.WorkerRepository;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
+@RefreshScope
 @RestController
 @RequestMapping("/workers")
 @Slf4j
@@ -32,10 +33,20 @@ public class WorkerResource {
         return ResponseEntity.ok(repository.findAll());
     }
 
+    @Value("${test.config}")
+    private String testConfig;
+
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
         log.info("Port = " + env.getProperty("local.server.port"));
 
         return ResponseEntity.ok(repository.findById(id).get());
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        log.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 }
