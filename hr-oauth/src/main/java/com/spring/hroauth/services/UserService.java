e546e1b6ddbce0e3846c1be7b4fbddd9ffa4fd30
Jonas <jonas@labsit.io>
2021-06-03 17:07:42 -0300

    Configurando autenticação

--- hr-oauth/src/main/java/com/spring/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/spring/hroauth/services/UserService.java
@@ -4,11 +4,14 @@ import com.spring.hroauth.entities.User;
 import com.spring.hroauth.feignclients.UserFeignClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignClient userFeignClient;
@@ -21,6 +24,19 @@ public class UserService {
         }
 
         log.info("Email found: " + email);
+
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEMail(username).getBody();
+        if (user == null) {
+            log.error("Email not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        log.info("Email found: " + username);
 
         return user;
     }
